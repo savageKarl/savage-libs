6d9f8c7f17b8a514a0321cb0e69a11119e75c1d2
savage <2443005004@qq.com>
2023-09-06 01:06:32 +0800

    chore:  delete redundant files

--- build/rollup.config.ts
+++ build/rollup.config.ts
@@ -10,21 +10,21 @@ import resolve from '@rollup/plugin-node-resolve'
 // import terser from '@rollup/plugin-terser';
 // used to obfuscate your code
 // https://www.npmjs.com/package/rollup-obfuscator
-import { obfuscator } from 'rollup-obfuscator'
+import { obfuscator, RollupObfuscatorOptions } from 'rollup-obfuscator'
 // https://www.npmjs.com/package/@rollup/plugin-json
 // Convert JSON files to ES Modules.
 import json from '@rollup/plugin-json'
 import autoExternal from 'rollup-plugin-auto-external'
 // This is a plugin that lets you roll-up your .d.ts definition files.
 // https://www.npmjs.com/package/rollup-plugin-dts
-import { dts } from 'rollup-plugin-dts'
+// import { dts } from 'rollup-plugin-dts'
 // https://www.npmjs.com/package/rollup-plugin-typescript2
 import rpt2 from 'rollup-plugin-typescript2'
 
-import { rollupCommand } from 'savage-rollup-command'
+// import { rollupCommand } from 'savage-rollup-command'
 
-import { obfusctorConfig } from './obfusctorConfig.js'
-import { tsconfigDefaults } from './tsconfigDefaults.js'
+import { obfusctorConfig } from './obfusctorConfig'
+// import { tsconfigDefaults } from './tsconfigDefaults.js'
 
 const isPro = process.env.mode === 'pro'
 export default [
@@ -46,38 +46,38 @@ export default [
 			commonjs(), // parse the module of commonjs specifications
 			resolve(), // parse third-party lib, because rollup only can parse local module
 			json(),
-			rollupCommand({
-				buildEnd(ctx) {
-					ctx.run('cd tests && yarn test && npx link ..')
-				}
+			// rollupCommand({
+			// 	buildEnd(ctx: any) {
+			// 		ctx.run('cd tests && yarn test && npx link ..')
+			// 	}
+			// }),
+			rpt2({
+				// tsconfigDefaults
+				tsconfig: '../tsconfig.json'
 			}),
 			autoExternal(),
-			...(isPro ? [obfuscator(obfusctorConfig)] : []),
-			rpt2({
-				tsconfigDefaults
-			}),
-			...(isPro ? [] : [])
-		]
-	},
-	{
-		input: './dist/index.d.ts',
-		output: [{ file: 'dist/main.d.ts', format: 'es' }],
-		plugins: [
-			// When using this plug-in in rollup monitoring mode, input file cannot be change or delete, otherwise an error will be reported. This is a bug that cannot be solved.
-			// so i use the nodemon to watch target file
-			dts(),
-			rollupCommand({
-				closeBundle(ctx) {
-					ctx.del([
-						'dist/*',
-						'!dist/index.cjs',
-						'!dist/index.mjs',
-						'!dist/main.d.ts',
-						'!dist/index.cjs.map',
-						'!dist/index.mjs.map'
-					])
-				}
-			})
+			...(isPro ? [obfuscator(obfusctorConfig as RollupObfuscatorOptions)] : [])
 		]
 	}
+	// {
+	// 	input: './dist/index.d.ts',
+	// 	output: [{ file: 'dist/main.d.ts', format: 'es' }],
+	// 	plugins: [
+	// 		// When using this plug-in in rollup monitoring mode, input file cannot be change or delete, otherwise an error will be reported. This is a bug that cannot be solved.
+	// 		// so i use the nodemon to watch target file
+	// 		dts()
+	// 		// rollupCommand({
+	// 		// 	closeBundle(ctx) {
+	// 		// 		ctx.del([
+	// 		// 			'dist/*',
+	// 		// 			'!dist/index.cjs',
+	// 		// 			'!dist/index.mjs',
+	// 		// 			'!dist/main.d.ts',
+	// 		// 			'!dist/index.cjs.map',
+	// 		// 			'!dist/index.mjs.map'
+	// 		// 		])
+	// 		// 	}
+	// 		// })
+	// 	]
+	// }
 ]
