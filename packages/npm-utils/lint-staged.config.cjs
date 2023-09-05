dbed1bc67d7f81024a0e8605847181aafb6b2c48
savage <2443005004@qq.com>
2023-09-05 20:56:42 +0800

    chore:  delete redundant files

--- packages/npm-utils/lint-staged.config.cjs
+++
@@ -1,3 +0,0 @@
-module.exports = {
-	'src/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['prettier --write']
-}
--- packages/rollup-config/tests/test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from '@jest/globals'
-
-test('sum', () => {
-	expect(1 + 2).toBe(3)
-})
