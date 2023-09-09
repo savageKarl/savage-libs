6457480ef7950896c097ec78648548b5cad93d3e
savage <2443005004@qq.com>
2023-09-09 08:20:20 +0800

    chore: change eslint config & fix alias setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,7 @@
+const DOMGlobals = ['window', 'document']
+const NodeGlobals = ['module', 'require']
+
+// eslint-disable-next-line no-restricted-globals
 module.exports = {
 	extends: [
 		'eslint-config-standard',
@@ -22,7 +26,27 @@ module.exports = {
 	rules: {
 		'prettier/prettier': 'error',
 		'@typescript-eslint/ban-ts-comment': 0,
-		'tsdoc/syntax': 'warn'
+		'tsdoc/syntax': 'warn',
+		'no-debugger': 'error',
+		'no-unused-vars': [
+			'error',
+			// we are only using this rule to check for unused arguments since TS
+			// catches unused variables but not args.
+			{ varsIgnorePattern: '.*', args: 'none' }
+		],
+		// most of the codebase are expected to be env agnostic
+		'no-restricted-globals': ['error', ...DOMGlobals, ...NodeGlobals],
+
+		'no-restricted-syntax': [
+			'error',
+			// since we target ES2015 for baseline support, we need to forbid object
+			// rest spread usage in destructure as it compiles into a verbose helper.
+			'ObjectPattern > RestElement',
+			// tsc compiles assignment spread into Object.assign() calls, but esbuild
+			// still generates verbose helpers, so spread assignment is also prohiboted
+			'ObjectExpression > SpreadElement'
+			// 'AwaitExpression'
+		]
 	},
 	parserOptions: {
 		project: './tsconfig.json',
--- scripts/aliases.ts
+++ scripts/aliases.ts
@@ -1,9 +1,13 @@
-// @ts-check
 import { resolve } from 'node:path'
 
-console.log(resolve(`packages/${'savage-data-types'}/src/index.ts`))
+// console.log(resolve(`packages/${'savage-data-types'}/src/index.ts`))
 const entries = {
-	'savage-*': 'packages/savage-*/src/index.ts'
+	// 'savage-*': '../packages/savage-*/src/index.ts'
+	'savage*': resolve('packages/savage-/src/index.ts'),
+	'savage-data-types': resolve('packages/savage-data-types/src/index.ts'),
+	'savage-electron-ipc': resolve('packages/savage-electron-ipc/src/index.ts'),
+	'savage-react-store': resolve('packages/savage-react-store/src/index.ts'),
+	'savage-utils': resolve('packages/savage-utils/src/index.ts')
 }
 
 export { entries }
