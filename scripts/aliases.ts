9e38c958678720b09389d565f645770b7822c020
savage <2443005004@qq.com>
2023-09-22 19:18:25 +0800

    chore(vite-plugin-us): change tsconfig for setup alias

--- scripts/aliases.ts
+++ scripts/aliases.ts
@@ -1,13 +1,14 @@
 import { resolve } from 'node:path'
 
+console.log(resolve('packages/vite-plugin-us'))
+
 // console.log(resolve(`packages/${'savage-data-types'}/src/index.ts`))
 const entries = {
-	// 'savage-*': '../packages/savage-*/src/index.ts'
-	'savage*': resolve('packages/savage-/src/index.ts'),
 	'savage-data-types': resolve('packages/savage-data-types/src/index.ts'),
 	'savage-electron-ipc': resolve('packages/savage-electron-ipc/src/index.ts'),
 	'savage-react-store': resolve('packages/savage-react-store/src/index.ts'),
-	'savage-utils': resolve('packages/savage-utils/src/index.ts')
+	'savage-utils': resolve('packages/savage-utils/src/index.ts'),
+	'vite-plugin-us': resolve('packages/vite-plugin-us/src')
 }
 
 export { entries }
