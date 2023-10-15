b669b0a9454450c5020feb8ac33af4c67a35210e
savage <2443005004@qq.com>
2023-10-15 20:19:18 +0800

    feat: add new package savage-mdtable

---
+++ packages/savage-mdtable/README.md
@@ -0,0 +1,7 @@
+# Savage-mdtable
+
+> A library for generate markdown tables.
+
+# Documentation
+
+See [here](https://savage181855.github.io/savage-libs/savage-mdtable/modules)
---
+++ packages/savage-mdtable/__test__/index.test.ts
@@ -0,0 +1,70 @@
+import { mdtable } from 'savage-mdtable'
+import type { TableOptions } from 'savage-mdtable'
+
+const options: TableOptions = {
+	header: ['A', 'B', 'C'],
+	alignment: ['R', 'C', 'L'],
+	rows: [
+		['1', '2', '3'],
+		['4', '5', '6'],
+		['7', '8', '9']
+	]
+}
+
+test('basic table', () => {
+	const expected = `
+| A | B | C |
+|--:|:-:|:--|
+| 1 | 2 | 3 |
+| 4 | 5 | 6 |
+| 7 | 8 | 9 |
+    `.trim()
+
+	expect(mdtable(options)).toBe(expected)
+})
+
+test('table with extra padding', () => {
+	const expected = `
+|   A   |   B   |   C   |
+|------:|:-----:|:------|
+|   1   |   2   |   3   |
+|   4   |   5   |   6   |
+|   7   |   8   |   9   |
+    `.trim()
+
+	expect(mdtable(Object.assign({}, { padding: 3 }, options))).toBe(expected)
+})
+
+test('table with different alignments', () => {
+	const expected = `
+| A | B | C |
+|:--|:-:|--:|
+| 1 | 2 | 3 |
+| 4 | 5 | 6 |
+| 7 | 8 | 9 |
+    `.trim()
+
+	expect(
+		mdtable(
+			Object.assign({}, options, {
+				alignment: ['L', 'C', 'R']
+			})
+		)
+	).toBe(expected)
+})
+
+test('table with per-column padding', () => {
+	const expected = `
+| A | BB | CCC |
+|--:|:--:|:----|
+| 1 | 2  |  3  |
+| 4 | 5  |  6  |
+| 7 | 8  |  9  |
+`
+
+	console.log(mdtable(Object.assign(options, { header: ['A', 'BB', 'CCC'] })))
+
+	expect(mdtable(Object.assign(options, { header: ['A', 'BB', 'CCC'] }))).toBe(
+		expected.trim()
+	)
+})
