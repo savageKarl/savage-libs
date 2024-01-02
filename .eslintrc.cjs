4613569cea690ace1bd9072cfd6bf4e2528734dd
savage <2443005004@qq.com>
2024-01-02 15:09:47 +0800

    chore: change eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -40,11 +40,7 @@ module.exports = {
 			'error',
 			// since we target ES2015 for baseline support, we need to forbid object
 			// rest spread usage in destructure as it compiles into a verbose helper.
-			'ObjectPattern > RestElement',
-			// tsc compiles assignment spread into Object.assign() calls, but esbuild
-			// still generates verbose helpers, so spread assignment is also prohiboted
-			'ObjectExpression > SpreadElement'
-			// 'AwaitExpression'
+			'ObjectPattern > RestElement'
 		]
 	},
 	parserOptions: {
