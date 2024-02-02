4bb45339aef03b9dfb9347da87498a77ed4d3e0c
savage <2443005004@qq.com>
2024-02-02 22:05:02 +0800

    docs: change sidebar and doc.ts

--- scripts/doc.ts
+++ scripts/doc.ts
@@ -1,20 +1,21 @@
-import { readFileSync } from 'node:fs'
-import { resolve } from 'node:path'
+import { readFileSync, writeFileSync } from 'node:fs'
+import path, { resolve } from 'node:path'
 import { spawn } from 'cross-spawn'
 import matter from 'gray-matter'
 import chokidar from 'chokidar'
 import glob from 'fast-glob'
 import { capitalize, normalizePath, debounce } from 'savage-utils'
-import { generateFiles } from 'savage-node'
+import { generateFiles, readJsonFile, writeJsonFile } from 'savage-node'
 import { createLogger } from 'savage-log'
 import {
-	resolveCliOption,
-	resolveTargetPkgNames,
-	packagesRoot,
-	replaceTemplateVariable,
-	getPkgJson,
-	spliceTemplate,
-	projectRoot
+  resolveCliOption,
+  resolveTargetPkgNames,
+  packagesRoot,
+  replaceTemplateVariable,
+  getPkgJson,
+  spliceTemplate,
+  projectRoot,
+  stringify
 } from './utils'
 
 const docsPath = resolve(process.cwd(), 'docs')
@@ -23,182 +24,185 @@ const resolvedPkgNames = resolveTargetPkgNames(targetPkgNames, all)
 const log = createLogger({ label: 'scripts/doc' })
 
 async function generateSidebarConfig() {
-	const contents = await Promise.all(
-		resolvedPkgNames.map(async pkgName => {
-			const path = normalizePath(`${docsPath}/packages/${pkgName}/sidebar.json`)
+  const paths = resolvedPkgNames.map((pkgName) =>
+    normalizePath(`${docsPath}/packages/${pkgName}/sidebar.json`)
+  )
 
-			return readFileSync(path, { encoding: 'utf-8' })
-		})
-	)
+  const contents = paths.map((p) => readFileSync(p, { encoding: 'utf-8' }))
 
-	const sidebarFilePath = normalizePath(`${docsPath}/sidebar.json`)
-	const content = contents
-		.map(v => JSON.parse(v))
-		.reduce((x, y) => Object.assign(x, y), {} as Record<string, string>)
+  const sidebarFilePath = normalizePath(`${docsPath}/sidebar.json`)
+  const contentRecord = contents
+    .map((v) => JSON.parse(v))
+    .reduce((x, y) => Object.assign(x, y), {} as Record<string, string>)
 
-	generateFiles({ [sidebarFilePath]: JSON.stringify(content, null, 4) })
+  generateFiles({ [sidebarFilePath]: stringify(contentRecord) })
 
-	return contents
+  if (mode !== 'dev') return
+  chokidar.watch(paths).on('change', (p) => {
+    const fullSideBar = readJsonFile(sidebarFilePath)
+    const currentSidebar = readJsonFile(p)
+
+    writeJsonFile(sidebarFilePath, Object.assign(fullSideBar, currentSidebar))
+  })
 }
 
 async function generateRootIndex() {
-	try {
-		const template = {
-			layout: 'home',
-			hero: {
-				name: 'Documentions',
-				image: {
-					src: '/logo.png',
-					alt: 'VitePress'
-				}
-			},
-			features: [] as any
-		}
-		const pkgs = resolvedPkgNames.map(name => {
-			return getPkgJson(resolve(packagesRoot, name))
-		})
-		template.features = pkgs.map(pkg => ({
-			title: pkg.name,
-			details: pkg.description,
-			link: `/${pkg.name}/index`,
-			linkText: 'go on'
-		}))
+  try {
+    const template = {
+      layout: 'home',
+      hero: {
+        name: 'Documentions',
+        image: {
+          src: '/logo.png',
+          alt: 'VitePress'
+        }
+      },
+      features: [] as any
+    }
+    const pkgs = resolvedPkgNames.map((name) => {
+      return getPkgJson(resolve(packagesRoot, name))
+    })
+    template.features = pkgs.map((pkg) => ({
+      title: pkg.name,
+      details: pkg.description,
+      link: `/${pkg.name}/index`,
+      linkText: 'go on'
+    }))
 
-		const content = matter.stringify('', template)
-		const indexPath = resolve(projectRoot, 'docs', 'index.md')
-		generateFiles({ [indexPath]: content })
-	} catch (e) {
-		console.error('generateRootIndex', e)
-	}
+    const content = matter.stringify('', template)
+    const indexPath = resolve(projectRoot, 'docs', 'index.md')
+    generateFiles({ [indexPath]: content })
+  } catch (e) {
+    console.error('generateRootIndex', e)
+  }
 }
 
 async function generateIndex() {
-	async function handle(path: string, pkgName: string) {
-		const pkgJson = getPkgJson(resolve(packagesRoot, pkgName))
-		const template = await spliceTemplate(['commonHeader', 'doc'])
+  async function handle(path: string, pkgName: string) {
+    const pkgJson = getPkgJson(resolve(packagesRoot, pkgName))
+    const template = await spliceTemplate(['commonHeader', 'doc'])
 
-		const content = replaceTemplateVariable(template, {
-			capitalizeName: capitalize(pkgJson.name),
-			description: pkgJson.description,
-			name: pkgJson.name,
-			content: readFileSync(path, { encoding: 'utf-8' })
-		})
-		generateFiles({ [path.replace('doc.md', 'index.md')]: content })
-	}
+    const content = replaceTemplateVariable(template, {
+      capitalizeName: capitalize(pkgJson.name),
+      description: pkgJson.description,
+      name: pkgJson.name,
+      content: readFileSync(path, { encoding: 'utf-8' })
+    })
+    generateFiles({ [path.replace('doc.md', 'index.md')]: content })
+  }
 
-	const paths = await Promise.all(
-		resolvedPkgNames.map(async pkgName => {
-			const path = normalizePath(`${docsPath}/packages/${pkgName}/doc.md`)
-			handle(path, pkgName)
-			return path
-		})
-	)
+  const paths = await Promise.all(
+    resolvedPkgNames.map(async (pkgName) => {
+      const path = normalizePath(`${docsPath}/packages/${pkgName}/doc.md`)
+      handle(path, pkgName)
+      return path
+    })
+  )
 
-	if (mode !== 'dev') return
-	chokidar.watch(paths).on('change', async path => {
-		path = normalizePath(path)
-		const splitArr = path.split('/')
-		const pkgName = splitArr[splitArr.length - 2]
-		log.info(`${normalizePath(path)} has changed!`)
-		handle(path, pkgName)
-	})
+  if (mode !== 'dev') return
+  chokidar.watch(paths).on('change', async (path) => {
+    path = normalizePath(path)
+    const splitArr = path.split('/')
+    const pkgName = splitArr[splitArr.length - 2]
+    log.info(`${normalizePath(path)} has changed!`)
+    handle(path, pkgName)
+  })
 
-	return await Promise.all(paths)
+  return await Promise.all(paths)
 }
 
 async function generateRewrites() {
-	const rewritesFilePath = resolve(docsPath, 'rewrites.json')
-	generateFiles({ [rewritesFilePath]: '{}' })
+  const rewritesFilePath = resolve(docsPath, 'rewrites.json')
 
-	const isNotMarkdownFile = (p: string) => !/[\w\d-]+\.md$/.test(p)
-	const isDocFile = (p: string) => /doc.*?\.md$/.test(p.split('/').reverse()[0])
-	const getRewrites = (paths: string[]) =>
-		paths
-			.map(f => 'packages' + f.split('packages')[1])
-			.reduce(
-				(x, y) =>
-					Object.assign(x, {
-						[y]: y.replace('packages/', '')
-					}),
-				{} as Record<string, string>
-			)
+  const isNotMarkdownFile = (p: string) => !/[\w\d-]+\.md$/.test(p)
+  const isDocFile = (p: string) => /doc.*?\.md$/.test(p.split('/').reverse()[0])
+  const getRewrites = (paths: string[]) =>
+    paths
+      .map((f) => 'packages' + f.split('packages')[1])
+      .reduce(
+        (x, y) =>
+          Object.assign(x, {
+            [y]: y.replace('packages/', '')
+          }),
+        {} as Record<string, string>
+      )
 
-	if (mode === 'build') {
-		const paths = (
-			await glob.async(
-				normalizePath(resolve(docsPath, 'packages', '**', '*.md'))
-			)
-		).filter(p => !isDocFile(p))
+  if (mode === 'build') {
+    const paths = (
+      await glob.async(
+        normalizePath(resolve(docsPath, 'packages', '**', '*.md'))
+      )
+    ).filter((p) => !isDocFile(p))
 
-		const rewrites = getRewrites(paths)
-		generateFiles({ [rewritesFilePath]: JSON.stringify(rewrites, null, 4) })
-	}
+    const rewrites = getRewrites(paths)
+    generateFiles({ [rewritesFilePath]: JSON.stringify(rewrites, null, 4) })
+  }
 
-	if (mode !== 'dev') return undefined
-	const watcher = chokidar.watch(normalizePath(`${docsPath}/packages`))
+  if (mode !== 'dev') return undefined
+  const watcher = chokidar.watch(normalizePath(`${docsPath}/packages`))
 
-	const stack: string[] = []
+  const stack: string[] = []
 
-	const addRewrites = debounce(() => {
-		const paths = stack.slice().map(f => normalizePath(f))
-		stack.length = 0
+  const addRewrites = debounce(() => {
+    const paths = stack.slice().map((f) => normalizePath(f))
+    stack.length = 0
 
-		const rewrites = getRewrites(paths)
+    const rewrites = getRewrites(paths)
 
-		const pathRecord = JSON.parse(
-			readFileSync(rewritesFilePath, { encoding: 'utf-8' }) || '{}'
-		) as Record<string, string>
+    const pathRecord = JSON.parse(
+      readFileSync(rewritesFilePath, { encoding: 'utf-8' }) || '{}'
+    ) as Record<string, string>
 
-		const content = JSON.stringify(Object.assign(rewrites, pathRecord), null, 4)
-		generateFiles({ [rewritesFilePath]: content })
-	}, 500)
+    const content = JSON.stringify(Object.assign(rewrites, pathRecord), null, 4)
+    generateFiles({ [rewritesFilePath]: content })
+  }, 500)
 
-	watcher.on('add', async path => {
-		path = normalizePath(path)
+  watcher.on('add', async (path) => {
+    path = normalizePath(path)
+    if (isNotMarkdownFile(path) || isDocFile(path)) return
 
-		if (isNotMarkdownFile(path) || isDocFile(path)) return
-		stack.push(path)
-		addRewrites()
-	})
+    stack.push(path)
+    addRewrites()
+  })
 
-	watcher.on('unlink', path => {
-		if (isNotMarkdownFile(path)) return undefined
+  watcher.on('unlink', (path) => {
+    if (isNotMarkdownFile(path)) return
 
-		const shortPath = 'packages' + normalizePath(path).split('packages')[1]
-		const pathRecord = JSON.parse(
-			readFileSync(rewritesFilePath, { encoding: 'utf-8' })
-		) as Record<string, string>
+    const shortPath = 'packages' + normalizePath(path).split('packages')[1]
+    const pathRecord = JSON.parse(
+      readFileSync(rewritesFilePath, { encoding: 'utf-8' })
+    ) as Record<string, string>
 
-		Reflect.deleteProperty(pathRecord, shortPath)
-		generateFiles({
-			[rewritesFilePath]: JSON.stringify(pathRecord, null, 4)
-		})
-	})
+    Reflect.deleteProperty(pathRecord, shortPath)
+    generateFiles({
+      [rewritesFilePath]: JSON.stringify(pathRecord, null, 4)
+    })
+  })
 }
 
 main()
 async function main() {
-	if (!['dev', 'preview', 'build'].includes(mode)) {
-		log.error(
-			'mode must be specified as dev, preview or build, receive',
-			`"${mode}"`
-		)
-		process.exit()
-	}
+  if (!['dev', 'preview', 'build'].includes(mode)) {
+    log.error(
+      'mode must be specified as dev, preview or build, receive',
+      `"${mode}"`
+    )
+    process.exit()
+  }
 
-	await generateRootIndex()
-	await generateSidebarConfig()
-	await generateIndex()
-	await generateRewrites()
+  await generateRootIndex()
+  await generateSidebarConfig()
+  await generateIndex()
+  await generateRewrites()
 
-	const res = spawn('pnpm', ['-F', 'docs', `docs:${mode}`])
-	res.stdout.on('data', res => console.log(String(res)))
+  // const res = spawn('pnpm', ['-F', 'docs', `docs:${mode}`])
+  // res.stdout.on('data', (res) => console.log(String(res)))
 
-	res.stderr.on('error', err => {
-		log.error('command execute error', JSON.stringify(err, null, 4))
-	})
+  // res.stderr.on('error', (err) => {
+  //   log.error('command execute error', JSON.stringify(err, null, 4))
+  // })
 
-	res.on('error', err => {
-		log.error('command not found error', JSON.stringify(err, null, 4))
-	})
+  // res.on('error', (err) => {
+  //   log.error('command not found error', JSON.stringify(err, null, 4))
+  // })
 }
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -20,147 +20,151 @@ export const packagesRoot = resolve(__dirname, '../packages')
 export const projectRoot = resolve(packagesRoot, '..')
 
 export function getFullpath(pkgName: string, subPath = 'src/index.ts') {
-	return normalizePath(resolve(packagesRoot, pkgName, subPath))
+  return normalizePath(resolve(packagesRoot, pkgName, subPath))
 }
 
 export function getFolderByPath(path: string) {
-	const folders: string[] = []
-	fs.readdirSync(path).forEach(f => {
-		if (fs.statSync(`${path}/${f}`).isDirectory()) {
-			folders.push(f)
-		}
-	})
+  const folders: string[] = []
+  fs.readdirSync(path).forEach((f) => {
+    if (fs.statSync(`${path}/${f}`).isDirectory()) {
+      folders.push(f)
+    }
+  })
 
-	return folders
+  return folders
 }
 
 /** sub package name of packages path */
-export const pkgNames = fs.readdirSync(packagesRoot).filter(f => {
-	if (!fs.statSync(`${packagesRoot}/${f}`).isDirectory()) return false
+export const pkgNames = fs.readdirSync(packagesRoot).filter((f) => {
+  if (!fs.statSync(`${packagesRoot}/${f}`).isDirectory()) return false
 
-	const pkg = require(`${packagesRoot}/${f}/package.json`)
-	if (pkg.private && !pkg.buildOptions) return false
+  const pkg = require(`${packagesRoot}/${f}/package.json`)
+  if (pkg.private && !pkg.buildOptions) return false
 
-	return true
+  return true
 })
 
 export function getChangedPkgNames() {
-	const result = spawn.sync('git', ['status', '-s'])
-	const stdout = result.stdout.toString()
+  const result = spawn.sync('git', ['status', '-s'])
+  const stdout = result.stdout.toString()
 
-	const matchs = [...stdout.matchAll(/\bpackages\/(?<pkgName>[\w-]+)\//g)]
+  const matchs = [...stdout.matchAll(/\bpackages\/(?<pkgName>[\w-]+)\//g)]
 
-	return [...new Set(matchs.map(v => v.groups?.pkgName))].filter(name =>
-		pkgNames.includes(name as string)
-	) as string[]
+  return [...new Set(matchs.map((v) => v.groups?.pkgName))].filter((name) =>
+    pkgNames.includes(name as string)
+  ) as string[]
 }
 
 export function fuzzyMatchPkgName(partialPkgNames: string[]) {
-	const matchResult = pkgNames
-		.concat(getPkgJson(projectRoot).name)
-		.filter(name => {
-			return partialPkgNames.some(v => name.match(v))
-		})
+  const matchResult = pkgNames
+    .concat(getPkgJson(projectRoot).name)
+    .filter((name) => {
+      return partialPkgNames.some((v) => name.match(v))
+    })
 
-	if (matchResult.length) return matchResult
+  if (matchResult.length) return matchResult
 
-	console.error(
-		[
-			pico.bgRed.white(' ERROR '),
-			'Target pkgName',
-			pico.red(JSON.stringify(partialPkgNames)),
-			'not Found!'
-		].join(' ')
-	)
+  console.error(
+    [
+      pico.bgRed.white(' ERROR '),
+      'Target pkgName',
+      pico.red(JSON.stringify(partialPkgNames)),
+      'not Found!'
+    ].join(' ')
+  )
 
-	process.exit()
+  process.exit()
 }
 
 export function getFilesByFolderSync(folerPath: string) {
-	const files: string[] = []
-	fs.readdirSync(folerPath).forEach(f => {
-		const fullpath = resolve(folerPath, f)
-		const stat = fs.statSync(fullpath)
-		if (!stat.isDirectory()) {
-			files.push(fullpath)
-		} else {
-			files.push(...getFilesByFolderSync(fullpath))
-		}
-	})
+  const files: string[] = []
+  fs.readdirSync(folerPath).forEach((f) => {
+    const fullpath = resolve(folerPath, f)
+    const stat = fs.statSync(fullpath)
+    if (!stat.isDirectory()) {
+      files.push(fullpath)
+    } else {
+      files.push(...getFilesByFolderSync(fullpath))
+    }
+  })
 
-	return files
+  return files
 }
 
 export function resolveCliOption(process: NodeJS.Process) {
-	const argv = minimist(process.argv.slice(2))
+  const argv = minimist(process.argv.slice(2))
 
-	const targetPkgNames: string[] = (argv.t || argv.target)?.split(',') || []
-	const watch: boolean = argv.watch || argv.w
-	const all: boolean = argv.all || argv.a
-	const mode: 'dev' | 'preview' | 'build' = argv.mode || argv.m
+  const targetPkgNames: string[] = (argv.t || argv.target)?.split(',') || []
+  const watch: boolean = argv.watch || argv.w
+  const all: boolean = argv.all || argv.a
+  const mode: 'dev' | 'preview' | 'build' = argv.mode || argv.m
 
-	return {
-		targetPkgNames,
-		watch,
-		all,
-		mode
-	}
+  return {
+    targetPkgNames,
+    watch,
+    all,
+    mode
+  }
 }
 
 export function resolveTargetPkgNames(targetPkgNames: string[], all: boolean) {
-	if (all) return pkgNames
+  if (all) return pkgNames
 
-	if (targetPkgNames.length) return fuzzyMatchPkgName(targetPkgNames)
+  if (targetPkgNames.length) return fuzzyMatchPkgName(targetPkgNames)
 
-	return getChangedPkgNames()
+  return getChangedPkgNames()
 }
 
 export function replaceTemplateVariable(
-	template: string,
-	variableRecord: Record<string, string>
+  template: string,
+  variableRecord: Record<string, string>
 ) {
-	return Object.keys(variableRecord).reduce(
-		(preV, curV) => preV.replaceAll(`[${curV}]`, variableRecord[curV]),
-		template
-	)
+  return Object.keys(variableRecord).reduce(
+    (preV, curV) => preV.replaceAll(`[${curV}]`, variableRecord[curV]),
+    template
+  )
 }
 
 export function getPkgJson(path: string) {
-	return require(resolve(path, 'package.json')) as Required<IPackageJson>
+  return require(resolve(path, 'package.json')) as Required<IPackageJson>
 }
 
 export const spliceTemplate = (() => {
-	const templateRecord: Record<string, string> = {}
+  const templateRecord: Record<string, string> = {}
 
-	return async function (names: string[]) {
-		if (Object.keys(templateRecord).length === 0) {
-			const templatePath = resolve(
-				process.cwd(),
-				'scripts/templates/documention'
-			)
-			const files = await readdir(templatePath)
+  return async function (names: string[]) {
+    if (Object.keys(templateRecord).length === 0) {
+      const templatePath = resolve(
+        process.cwd(),
+        'scripts/templates/documention'
+      )
+      const files = await readdir(templatePath)
 
-			const removeExt = (name: string) => name.replace(/\.\w+$/, '')
+      const removeExt = (name: string) => name.replace(/\.\w+$/, '')
 
-			;(
-				await Promise.all(
-					files.map(f =>
-						readFile(resolve(templatePath, f), { encoding: 'utf-8' })
-					)
-				)
-			).map((v, i) =>
-				Object.assign(templateRecord, {
-					[removeExt(files[i])]: v
-				})
-			)
-		}
+      ;(
+        await Promise.all(
+          files.map((f) =>
+            readFile(resolve(templatePath, f), { encoding: 'utf-8' })
+          )
+        )
+      ).map((v, i) =>
+        Object.assign(templateRecord, {
+          [removeExt(files[i])]: v
+        })
+      )
+    }
 
-		return names
-			.reduce((preV, curV) => preV.concat(templateRecord[curV]), [] as string[])
-			.join('\n')
-	}
+    return names
+      .reduce((preV, curV) => preV.concat(templateRecord[curV]), [] as string[])
+      .join('\n')
+  }
 })()
 
 export function normalizePath(path: string) {
-	return path.replaceAll('\\', '/')
+  return path.replaceAll('\\', '/')
+}
+
+export function stringify(value: unknown) {
+  return JSON.stringify(value, null, 4)
 }
