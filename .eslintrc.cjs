484f5e28ff4388b2664c03e0ea0541d6ff27e4d0
savage <2443005004@qq.com>
2024-02-02 11:12:11 +0800

    chore: change eslint  and prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,64 +2,63 @@ const NodeGlobals = ['module', 'require']
 
 // eslint-disable-next-line no-restricted-globals
 module.exports = {
-	extends: [
-		'eslint-config-standard',
-		'plugin:@typescript-eslint/recommended',
-		'prettier',
-		'plugin:prettier/recommended'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint/eslint-plugin',
-		'promise',
-		'tsdoc',
-		'jest',
-		'prettier'
-	],
-	root: true,
-	env: {
-		node: true,
-		es6: true,
-		commonjs: true
-	},
-	rules: {
-		'prettier/prettier': 'error',
-		'@typescript-eslint/ban-ts-comment': 0,
-		'tsdoc/syntax': 'warn',
-		'no-debugger': 'error',
-		'no-unused-vars': [
-			'error',
-			// we are only using this rule to check for unused arguments since TS
-			// catches unused variables but not args.
-			{ varsIgnorePattern: '.*', args: 'none' }
-		],
-		// most of the codebase are expected to be env agnostic
-		'no-restricted-globals': ['error', ...NodeGlobals],
-		'@typescript-eslint/no-explicit-any': 0
-	},
-	parserOptions: {
-		project: './tsconfig.json',
-		tsconfigRootDir: __dirname,
-		ecmaVersion: 2018,
-		sourceType: 'module'
-	},
-	overrides: [
-		{
-			files: ['**/__tests__/**'],
-			rules: {
-				'no-restricted-globals': 'off',
-				'no-restricted-syntax': 'off',
-				'jest/no-disabled-tests': 'error',
-				'jest/no-focused-tests': 'error'
-			}
-		},
-		// Node scripts
-		{
-			files: ['scripts/**', 'index.js', 'packages/**/index.js'],
-			rules: {
-				'no-restricted-globals': 'off',
-				'no-restricted-syntax': 'off'
-			}
-		}
-	]
+  extends: [
+    'eslint-config-standard',
+    'plugin:@typescript-eslint/recommended'
+    // 'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'promise',
+    'tsdoc',
+    'jest',
+    'prettier'
+  ],
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+    commonjs: true
+  },
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'tsdoc/syntax': 'warn',
+    'no-debugger': 'error',
+    'no-unused-vars': [
+      'error',
+      // we are only using this rule to check for unused arguments since TS
+      // catches unused variables but not args.
+      { varsIgnorePattern: '.*', args: 'none' }
+    ],
+    // most of the codebase are expected to be env agnostic
+    'no-restricted-globals': ['error', ...NodeGlobals],
+    '@typescript-eslint/no-explicit-any': 0
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**'],
+      rules: {
+        'no-restricted-globals': 'off',
+        'no-restricted-syntax': 'off',
+        'jest/no-disabled-tests': 'error',
+        'jest/no-focused-tests': 'error'
+      }
+    },
+    // Node scripts
+    {
+      files: ['scripts/**', 'index.js', 'packages/**/index.js'],
+      rules: {
+        'no-restricted-globals': 'off',
+        'no-restricted-syntax': 'off'
+      }
+    }
+  ]
 }
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,16 +1,109 @@
+/**
+ * @see https://prettier.io/docs/en/options.html#print-width
+ * @author lcm
+ */
 module.exports = {
-	endOfLine: 'auto',
-	printWidth: 80, //（默认值）单行代码超出 80 个字符自动换行
-	tabWidth: 2, //（默认值）一个 tab 键缩进相当于 2 个空格
-	useTabs: true, // 行缩进不使用 tab 键代替空格
-	semi: false, //（默认值）禁止语句的末尾加上分号
-	singleQuote: true, // 使用单引号
-	quoteProps: 'as-needed', //（默认值）仅仅当必须的时候才会加上双引号
-	jsxSingleQuote: true, // 在 JSX 中使用单引号
-	trailingComma: 'none', // 不用在多行的逗号分隔的句法结构的最后一行的末尾加上逗号
-	bracketSpacing: true, //（默认值）在括号和对象的文字之间加上一个空格
-	jsxBracketSameLine: true, // 把 > 符号放在多行的 JSX 元素的最后一行
-	arrowParens: 'avoid', // 当箭头函数中只有一个参数的时候可以忽略括弧
-	vueIndentScriptAndStyle: false, //（默认值）对于 .vue 文件，不缩进 <script> 和 <style> 里的内容
-	embeddedLanguageFormatting: 'off' // 不允许格式化内嵌的代码块，比如 markdown  文件里的代码块
+  /**
+   * 换行宽度，当代码宽度达到多少时换行
+   * @default 80
+   * @type {number}
+   */
+  printWidth: 80,
+  /**
+   * 缩进的空格数量
+   * @default 2
+   * @type {number}
+   */
+  tabWidth: 2,
+  /**
+   * 是否使用制表符代替空格
+   * @default false
+   * @type {boolean}
+   */
+  useTabs: false,
+  /**
+   * 是否在代码块结尾加上分号
+   * @default true
+   * @type {boolean}
+   */
+  semi: false,
+  /**
+   * 是否使用单引号替代双引号
+   * @default false
+   * @type {boolean}
+   */
+  singleQuote: true,
+  /**
+   * 对象属性的引号处理
+   * @default "as-needed"
+   * @type {"as-needed"|"consistent"|"preserve"}
+   */
+  quoteProps: 'as-needed',
+  /**
+   * jsx中是否使用单引号替代双引号
+   * @default false
+   * @type {boolean}
+   */
+  jsxSingleQuote: true,
+  /**
+   * 在jsx中使用是否单引号代替双引号
+   * @default false
+   * @type {boolean}
+   */
+  /**
+   * 末尾是否加上逗号
+   * @default "es5"
+   * @type {"es5"|"none"|"all"}
+   */
+  trailingComma: 'none',
+  /**
+   * 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+   * @default true
+   * @type {boolean}
+   */
+  bracketSpacing: true,
+  /**
+   * 把多行HTML (HTML, JSX, Vue, Angular)元素的>放在最后一行的末尾，而不是单独放在下一行(不适用于自关闭元素)。
+   * @default false
+   * @type {boolean}
+   */
+  bracketSameLine: false,
+  /**
+   * 当箭头函数只有一个参数是否加括号
+   * @default "always"
+   * @type {"always"|"avoid"}
+   */
+  arrowParens: 'always',
+  /**
+   * 为HTML、Vue、Angular和Handlebars指定全局空格敏感性
+   * @default "css"
+   * @type {"css"|"strict"|"ignore"}
+   */
+  htmlWhitespaceSensitivity: 'ignore',
+  /**
+   * 是否缩进Vue文件中的<script>和<style>标记内的代码。有些人(比如Vue的创建者)不使用缩进来保存缩进级别，但这可能会破坏编辑器中的代码折叠。
+   * @default "always"
+   * @type {"always"|"avoid"}
+   */
+  vueIndentScriptAndStyle: false,
+  /**
+   * 文件结束符
+   * @default "lf"
+   * @type {"lf"|"crlf"|"cr"|"auto"}
+   */
+  // endOfLine: 'crlf',
+  /**
+   * 因为使用了一些折行敏感型的渲染器（如GitHub comment）而按照markdown文本样式进行折行
+   */
+  proseWrap: 'never',
+  // 是否使用根目录下的EditorConfig配置文件
+  useEditorConfig: false,
+  /**
+   * HTML\VUE\JSX每行只有单个属性
+   * @default true
+   * @type {boolean}
+   */
+  singleAttributePerLine: true,
+  disableLanguages: ['html']
 }
+
