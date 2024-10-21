9025638cdecae4ce7ddce0e45e26cefd771c7d9b
savage <2443005004@qq.com>
2024-10-21 14:21:45 +0800

    chore(vite-plugin-us): change playground config

--- packages/vite-plugin-us/playground/vue-ts/vite.config.ts
+++ packages/vite-plugin-us/playground/vue-ts/vite.config.ts
@@ -22,9 +22,11 @@ export default defineConfig({
         version: '1',
         author: 'savage',
         description: 'developing plugin',
-        include: ['http://localhost:5501/packages/vite-plugin-us/index.html'],
+        include: [
+          'http://localhost:5502/packages/vite-plugin-us/index.html',
+          'https://github.com/vuejs/vue/releases?page=4'
+        ],
         match: ['https://translate.google.com/*', ''],
-        // runAt: '',
         runAt: 'document_start',
         require: [
           // 'https://unpkg.com/vue@3'
@@ -33,14 +35,22 @@ export default defineConfig({
       },
       server: {
         port: 12345
-        // open: false
       },
       build: {
         minify: false,
-        external: {
-          autoCDN: true
-          // exclusions: ['element-plus']
+        open: {
+          enable: true
         },
+        external: {
+          globals: [
+            {
+              pkgName: 'vue',
+              globalVariableName: 'Vue',
+              url: 'https://registry.npmmirror.com/vue/3.5.12/files/dist/vue.global.prod.js'
+            }
+          ]
+        },
+
         cssMinify: false
       },
       generate: {
